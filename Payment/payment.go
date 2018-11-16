c9b4b32de4369c3e132b23bd3184964c8e6cf3ba
Sheethal Halandur Nagaraja <35901807+Sheethalhn@users.noreply.github.com>
2018-11-15 23:13:02 -0800

    PaymentAPI_startercode

---
+++ Payment/payment.go
@@ -0,0 +1,70 @@
+/*
+	Payment API in Go
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+	"github.com/codegangsta/negroni"
+	"github.com/streadway/amqp"
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+	"github.com/satori/go.uuid"
+	"gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
+)
+'
+
+// NewServer configures and returns a Server.
+func NewServer() *negroni.Negroni {
+	formatter := render.New(render.Options{
+		IndentJSON: true,
+	})
+	n := negroni.Classic()
+	mx := mux.NewRouter()
+	initRoutes(mx, formatter)
+	n.UseHandler(mx)
+	return n
+}
+
+// API Routes
+func initRoutes(mx *mux.Router, formatter *render.Render) {
+	mx.HandleFunc("/ping", pingHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/order", newPaymentHandler(formatter)).Methods("POST")
+}
+
+// Helper Functions
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+// API Ping Handler
+func pingHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, struct{ Test string }{"API version 1.0 alive!"})
+	}
+}
+
+
+
+// API to Create New Payment request
+func newPaymentHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		uuid, _ := uuid.NewV4()
+}
+
+
+
+
+
+
+
+
+
