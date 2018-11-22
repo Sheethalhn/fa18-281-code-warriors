63bd7b1117a5097962f82cb576aefaaa073fc601
Sheethal Halandur Nagaraja <35901807+Sheethalhn@users.noreply.github.com>
2018-11-21 23:27:39 -0800

    Updating API route changes

--- Inventory/server.go
+++ Inventory/server.go
@@ -31,3 +31,52 @@ func NewServer() *negroni.Negroni {
 	n.UseHandler(mx)
 	return n
 }
+
+// API Routes
+func initRoutes(mx *mux.Router, formatter *render.Render) {
+	mx.HandleFunc("/ping", pingHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/inventory/{bookids}", getInventoryHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/inventory/{bookids}", updateInventoryHandler(formatter)).Methods("POST")
+}
+
+func getInventoryHandler(formatter *render.Render) http.HandlerFunc{
+	return func(w http.ResponseWriter, req *http.Request) {	
+	var bookIds []string = strings.Fields(req.URL.Query().Get("bookIds"))
+	fmt.Println("bookIds", bookIds )
+
+	session, err := mgo.Dial(mongodb_server)
+        if err != nil {
+                panic(err)
+        }
+        defer session.Close()
+        session.SetMode(mgo.Monotonic, true)
+        c := session.DB(mongodb_database).C(mongodb_collection)
+        var result bool
+		oids := make([]bson.ObjectId, len(bookIds))
+		for i := range bookIds {
+		  oids[i] = bson.ObjectIdHex(bookIds[i])
+		}
+		query := bson.M{"_id": bson.M{"$in": bookIds}}
+		err = c.Find(query).All(&result)
+		rawjson, err := json.Marshal(result)
+		var bookResults string = string(rawjson) 
+		if err == nil {
+				formatter.JSON(w, http.StatusOK, result)
+		} else {
+			log.Fatal(err)
+		}
+	}
+}
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+// API Ping Handler
+func pingHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, struct{ Test string }{"API version 1.0 alive!"})
+	}
+}
