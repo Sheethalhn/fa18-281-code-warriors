7b722a829d56949f801bc2876a739e52b88846a2
Sheethal Halandur Nagaraja <35901807+Sheethalhn@users.noreply.github.com>
2018-11-21 22:01:56 -0800

    update server.go

--- Inventory/server.go
+++ Inventory/server.go
@@ -1,1 +1,33 @@
+package main
 
+import (
+	"fmt"
+	"strings"
+	"log"
+	"net/http"
+	"encoding/json"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+	"gopkg.in/mgo.v2"
+    	"gopkg.in/mgo.v2/bson"
+)
+
+var mongodb_server = "localhost"
+var mongodb_database = "Bookstore"
+var mongodb_collection = "books"
+
+
+// NewServer configures and returns a Server.
+func NewServer() *negroni.Negroni {
+	     formatter := render.New(render.Options{
+		          IndentJSON: true,
+	})
+	
+
+	n := negroni.Classic()
+	mx := mux.NewRouter()
+	initRoutes(mx, formatter)
+	n.UseHandler(mx)
+	return n
+}
