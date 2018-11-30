a116ca9b5afbd0ee7f1fa3261e3941a7b1343b57
harshrp2511github <harshrp2511@gmail.com>
2018-11-29 19:49:40 -0800

    Signup Page for React

--- Bookstore/client/src/components/Login/Login.js
+++ Bookstore/client/src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './Design.css'
+import Header from '../Header/Header';
 import bookstore from './images/book_store.jpg';
 import { Link } from 'react-router-dom';
 
@@ -10,12 +11,13 @@ class Login extends Component{
     render(){
         return(
             <div className="container">
+                <Header/>
                 <div className="login-box col-md-4">
                     <img src={bookstore} className="book-style" />
-                    <p style={{ color: "white"}}>LOGIN</p> <br />
+                    <p style={{ color: "white"}}>LOGIN HERE</p> <br />
                     <div className="form-body">
-                        <input type="text" placeholder="username" className="form-element"/>
-                        <input  type="password" placeholder= "password" className="form-element"/>
+                        <input type="text" placeholder="Username" className="form-element"/>
+                        <input  type="password" placeholder= "Password" className="form-element"/>
 
                     </div>
                     <button type="button" className="btn button-design">SIGNIN</button>
--- Bookstore/client/src/components/Login/Signup.js
+++ Bookstore/client/src/components/Login/Signup.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import './Design.css'
+import bookstore from './images/book_store.jpg';
+import { Link } from 'react-router-dom';
+import Header from '../Header/Header';
 
 
 class Signup extends Component{
@@ -6,8 +10,23 @@ class Signup extends Component{
 
     render(){
         return(
-            <div>
-                Signup here!!
+            <div className="container">
+                <Header/>
+                <div className="signup-box col-md-4">
+                    <img src={bookstore} className="book-style" />
+                    <p style={{ color: "white"}}>SIGNUP HERE</p> <br />
+                    <div className="form-body">
+                        <input type="text" placeholder="Username" className="form-element"/>
+                        <input  type="text" placeholder= "First Name" className="form-element"/>
+                        <input  type="text" placeholder= "Last Name" className="form-element"/>
+                        <input  type="password" placeholder= "Password" className="form-element"/>
+                        <input  type="password" placeholder= "Confirm Password" className="form-element"/>
+
+                    </div>
+                    <button type="button" className="btn button-design">SIGNUP</button>
+                    <br /><br />
+                    <p style={{ color: "white" }}>Already a Member? <Link to="/login">Login here!!</Link> </p>
+                </div>
 
             </div>
 
