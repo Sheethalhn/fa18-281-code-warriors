7e5a7c80db32c0cf00e9186f2308278989a3e8d0
harshrp2511github <harshrp2511@gmail.com>
2018-11-29 19:38:24 -0800

    Login Page for React

--- Bookstore/client/src/components/Login/Login.js
+++ Bookstore/client/src/components/Login/Login.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import './Design.css'
+import bookstore from './images/book_store.jpg';
+import { Link } from 'react-router-dom';
 
 
 class Login extends Component{
@@ -6,8 +9,19 @@ class Login extends Component{
 
     render(){
         return(
-            <div>
-                Login here!!
+            <div className="container">
+                <div className="login-box col-md-4">
+                    <img src={bookstore} className="book-style" />
+                    <p style={{ color: "white"}}>LOGIN</p> <br />
+                    <div className="form-body">
+                        <input type="text" placeholder="username" className="form-element"/>
+                        <input  type="password" placeholder= "password" className="form-element"/>
+
+                    </div>
+                    <button type="button" className="btn button-design">SIGNIN</button>
+                    <br /><br />
+                    <p style={{ color: "white" }}>Not a Member Already? <Link to="/signup">Signup here!!</Link> </p>
+                </div>
 
             </div>
 
