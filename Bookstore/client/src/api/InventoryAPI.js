0037faef51af4efa1739501bfddaf7b7c57c6d93
sheethalhn <sheethal.halandurnagaraja@sjsu.edu>
2018-11-30 22:28:01 -0800

    Updating Payment front end and Inventory API changes

---
+++ Bookstore/client/src/api/InventoryAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const api = 'http://localhost:3000'
+
+export const viewInventory = (payload) =>
+    axios.post(api+`/viewinventory`+payload)
+        .then(response => {
+            return response;
+        }).catch(error => {
+        return error;
+    });
+
+export const updateInventory = (payload) =>
+    axios.post(api+`/updateinventory`+payload)
+        .then(response => {
+            return response;
+        }).catch(error => {
+        return error;
+    });
