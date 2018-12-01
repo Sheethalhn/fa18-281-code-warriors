281f1eac2f767ddfc02c66f35ba7c59ce26f3c68
Divyank Shukla <31362002+divyank68@users.noreply.github.com>
2018-12-01 14:32:11 -0800

    updated fetch transactions by user

--- Transaction/client/src/components/Transaction/Transaction.js
+++ Transaction/client/src/components/Transaction/Transaction.js
@@ -9,21 +9,16 @@ class Transaction extends Component{
     constructor(props){
         super(props);
         this.state={
-            Username: '',
-            Password: '',
-            C_Password: '',
-            FirstName: '',
-            LastName: '',
-            message: ''
+            transactions: []
         }
     }
 
     componentWillMount() {
         axios.get('http://localhost:3000/getAllTransactionByUser/12345').then((response) => {
-            console.log(response);
-            if(response.data == "true"){
+            console.log(response.data);
+            if(response.status === 200){
                 this.setState({
-                    message: "get tran Successfull!"
+                    transactions: response.data
                 })
             }
 
@@ -31,35 +26,19 @@ class Transaction extends Component{
     }
 
 
-    handleSignUp = (data) => {
-
-        var config = {
-            // headers: {'Access-Control-Allow-Origin': '*',
-            //     'Accept': 'application/json'
-            // }
-        };
-
-
-    }
 
 
     render(){
         return(
             <div className="container">
                 <Header/>
-                <div className="signup-box col-md-4">
+
                     <p style={{ color: "white"}}>Recent Transactions</p> <br />
-                    <div className="form-body">
-                        <input type="text" placeholder="Username" className="form-element"
-                               onChange={(event) => {
-                                   this.setState({
-                                       Username: event.target.value,
-                                       type: true
-                                   });
-                               }}
-                        />
-                </div>
-			</div>
+                    {this.state.transactions.map((transaction) => (
+                        <div key={transaction.id} >{transaction.id}</div>
+                    ))}
+
+
             </div>
 
 
