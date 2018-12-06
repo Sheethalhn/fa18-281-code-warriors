e47d252e0868af8863016e2749bea3a0ac25ea3c
Divyank Shukla <31362002+divyank68@users.noreply.github.com>
2018-12-05 22:22:18 -0800

    update transaction

--- Bookstore/client/src/components/Transaction/Transaction.js
+++ Bookstore/client/src/components/Transaction/Transaction.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Header from '../Header/Header';
 import axios from 'axios'
+import './Design.css';
 
 
 class Transaction extends Component{
@@ -8,23 +9,41 @@ class Transaction extends Component{
     constructor(props){
         super(props);
         this.state={
-            transactions: []
+            transactions: [],
+            noTran:""
         }
     }
 
     componentWillMount() {
-        axios.get('http://localhost:3000/getAllTransactionByUser/12345').then((response) => {
-            console.log(response.data);
-            if(response.status === 200){
+        axios.get('http://localhost:3000/getAllTransactionByUser/1234').then((response) => {
+            console.log("dfjfd",response.data);
+            if(response.status === 200 && response.data!==null){
                 this.setState({
                     transactions: response.data
                 })
             }
+            else{
+                this.setState({
+                    noTran: "Sorry, Please purchase some books to see transaction"
+                })
+            }
+
 
         })
     }
 
-
+    renderBooks = (transaction) => {
+        return transaction.books.map(book =>{
+            return(
+            <div>
+                BookName: {book.bookname}<br/>
+                Qty: {book.qty}<br/>
+                Price: {book.price}<br/>
+                <hr />
+            </div>
+            )
+        })
+    }
 
 
     render(){
@@ -32,17 +51,19 @@ class Transaction extends Component{
             <div className="container">
                 <Header/>
 
-                    <p style={{ color: "white"}}>Recent Transactions</p> <br />
+                    <p style={{ color: "black"}}>Recent Transactions</p> <br />
+
                     {this.state.transactions.map((transaction) => (
-                        <div key={transaction.id} >
-                            {transaction.id}<br/>
-                            Books: {transaction.books.map((book) => (
-                            <div key={book.id} >
-                                {book.bookid}<br/>
+                        <div key={transaction.id} className="transaction" >
+                            <div className="sub-transaction">
+                            <p>Transaction Id: {transaction.id}</p>
+                                {this.renderBooks(transaction)}
+                                <p> Total Transaction amount: {transaction.totalamount}</p>
                             </div>
-                            ))}
                         </div>
                     ))}
+
+                {this.state.noTran}
 
 
             </div>
