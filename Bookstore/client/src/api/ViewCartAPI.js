4302ced8abc6983ff85a42f384985ed23f9cfea5
Divyank Shukla <31362002+divyank68@users.noreply.github.com>
2018-12-07 21:50:51 -0800

    Update ViewCartAPI.js

--- Bookstore/client/src/api/ViewCartAPI.js
+++ Bookstore/client/src/api/ViewCartAPI.js
@@ -47,13 +47,11 @@ export const updateCart = (userid, data) =>
     });
 
 // Clear cart after Payment is made
-export const clearCart = (userid) => {
-    var data = {}
-    axios.post(api+`/clearcart/${userid}`,data, req_header)
+export const clearCart = (userid) =>
+    axios.post(api+`/clearcart/${userid}`,{}, req_header)
         .then(response => {
             return response;
         }).catch(error => {
         return error;
     });
-}
 
